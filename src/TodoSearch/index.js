9ad5003834cb5b262334adfeb33c242a9c6ad12a
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-11-17 23:43:24 -0300

    se añadieron los estilos personales al proyecto y se finaliza para producción

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -10,7 +10,7 @@ function TodoSearch(){
 
     return(
       <input 
-      placeholder="Cortar cebolla"
+      placeholder="¡Construye tu futuro ahora!"
       className='TodoSearch'
       value={searchValue}
       onChange={(event)=>{
